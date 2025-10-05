141df3f72cc5f93824278dd17ecf590fd96bd8be
electh <noteperson@gmail.com>
2025-10-05 18:22:53 +0800

    UI tweaks

--- src/components/ArticleList/components/EmptyPlaceholder.jsx
+++ src/components/ArticleList/components/EmptyPlaceholder.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 export default function EmptyPlaceholder() {
   const { t } = useTranslation();
   return (
-    <div className="h-full w-full bg-background rounded-lg shadow-custom">
+    <div className="h-full w-full bg-background rounded-2xl shadow-custom">
       <div className="flex flex-col items-center gap-2 w-full justify-center h-full text-default-400">
         <Inbox className="size-16" />
         {t("articleList.emptyPlaceholder")}
